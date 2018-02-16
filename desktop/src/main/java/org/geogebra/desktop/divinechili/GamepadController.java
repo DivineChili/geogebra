00a0fdb7c71cb6bf17be074a2c3cce5ab542764e
Simon Andreas Bjørn <simon.a@unseen.is>
2018-02-16 09:19:19 +0100

    * Creates a security loop to detect if controllers are disconnected and/or reconnected

--- desktop/src/main/java/org/geogebra/desktop/divinechili/GamepadController.java
+++ desktop/src/main/java/org/geogebra/desktop/divinechili/GamepadController.java
@@ -11,35 +11,49 @@ public class GamepadController implements Runnable
     public void run() {
         ControllerManager controllers = new ControllerManager();
         controllers.initSDLGamepad();
+        while (true) {
+            while (true) {
+                ControllerState currState = controllers.getState(0);
 
-        while(true) {
-            ControllerState currState = controllers.getState(0);
+                if (!currState.isConnected) {
+                    System.out.println("Controller is not connected!");
+                    break;
+                }
 
-            if(!currState.isConnected) {
-                System.out.println("Controller is not connected!");
-                break;
+                // Axis-events
+                // Left Joystick is outside of idle zone
+                if (currState.leftStickY > tolerance || currState.leftStickY < -tolerance ||
+                        currState.leftStickX > tolerance || currState.leftStickX < -tolerance) {
+                    System.out.println("Left  Joystick { X: " + currState.leftStickX + "\tY: " + currState.leftStickY);
+                    // TODO: Add event dispatcher for listeners here
+                }
+                // Right Joystick is outside of idle zone
+                if (currState.rightStickY > tolerance || currState.rightStickY < -tolerance ||
+                        currState.rightStickX > tolerance || currState.rightStickX < -tolerance) {
+                    System.out.println("Right Joystick { X: " + currState.rightStickX + "\tY: " + currState.rightStickY);
+                    // TODO: Add event dispatcher for listeners here
+                }
+
+
+                // Button-events
+                // Right
+
             }
-
-            // Axis-events
-            // Left Joystick is outside of idle zone
-            if(currState.leftStickY > tolerance || currState.leftStickY < -tolerance ||
-                    currState.leftStickX >tolerance || currState.leftStickX < -tolerance)
-            {
-                System.out.println("Left  Joystick { X: " + currState.leftStickX + "\tY: " + currState.leftStickY);
-                // TODO: Add event dispatcher for listeners here
+            while (true) {
+                controllers = new ControllerManager();
+                controllers.initSDLGamepad();
+                if (controllers.getNumControllers() > 0) {
+                    System.out.println("Controller detected!");
+                    break;
+                } else {
+                    System.out.println("No controller detected...");
+                }
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-            // Right Joystick is outside of idle zone
-            if (currState.rightStickY > tolerance || currState.rightStickY < -tolerance ||
-                    currState.rightStickX > tolerance || currState.rightStickX < -tolerance)
-            {
-                System.out.println("Right Joystick { X: " + currState.rightStickX + "\tY: " + currState.rightStickY);
-                // TODO: Add event dispatcher for listeners here
-            }
-
-
-            // Button-events
-            // Right
-
         }
     }
 }
